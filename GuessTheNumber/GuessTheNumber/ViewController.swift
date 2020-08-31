375a89308e3d11551243f14007ea8293ba8dbe26
Kaavya Singhal <kaavya@gatech.edu>
2020-08-31 16:40:35 -0400

    First commit

---
+++ GuessTheNumber/GuessTheNumber/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  GuessTheNumber
+//
+//  Created by Kaavya Singhal on 8/31/20.
+//  Copyright © 2020 Kaavya Singhal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let lowerBound = 1
+    let upperBound = 100
+    var numberToGuess: Int!
+    var numberofGuesses = 0
+    
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBOutlet weak var guessLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        generateRandomNumber()
+    }
+    
+    func generateRandomNumber() {
+        numberToGuess = Int(arc4random_uniform(100)) + 1
+    }
+    
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        if let guess = guessTextField.text {
+            if let guessInt = Int(guess) {
+                numberofGuesses = numberofGuesses + 1
+                validateGuess(guessInt)
+            }
+            
+        }
+    }
+    
+    func validateGuess(_ guess: Int) {
+        if guess < lowerBound || guess > upperBound {
+            showBoundsAlert()
+        } else if guess < numberToGuess {
+            print("Higher!")
+        } else if guess > numberToGuess {
+            guessLabel.text = "Lower!"
+        } else {
+            showWinAlert()
+            guessLabel.text = "Guess the Number"
+            numberofGuesses = 0
+            generateRandomNumber()
+        }
+    }
+    
+    func showBoundsAlert() {
+        let alert = UIAlertController(title: "Hey!", message : "Your guess should be between 1 and 100!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Got it", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func showWinAlert() {
+        let alert = UIAlertController(title: "Congrats!", message: "You won with a total of \(numberofGuesses) guesses", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Play again", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+
+}
+
