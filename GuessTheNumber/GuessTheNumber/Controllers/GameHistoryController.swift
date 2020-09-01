2cb07f4ea941311c13a1dcad5e0dd0e95b7bffb1
Kaavya Singhal <kaavya@gatech.edu>
2020-09-01 12:59:35 -0400

    Third commit

---
+++ GuessTheNumber/GuessTheNumber/Controllers/GameHistoryController.swift
@@ -0,0 +1,30 @@
+//
+//  GameHistoryController.swift
+//  GuessTheNumber
+//
+//  Created by Kaavya Singhal on 9/1/20.
+//  Copyright © 2020 Kaavya Singhal. All rights reserved.
+//
+
+import UIKit
+
+class GameHistoryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
